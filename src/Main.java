b352e70dd315f9220ed993600d40bfc63d047263
GuilhermeSerafim <guilerstudies@gmail.com>
2023-10-23 10:49:29 -0300

    Declarando variaveis, utilizando text block

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,26 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        int num = 5;
+        int resultado = num++; //num é atribuído primeiramente à variável resultado e depois incrementado para 6
+        System.out.println(num); // imprime 6
+        System.out.println(resultado); // imprime 5
+        double media = (9.8 + 6.3 + 8) / 3;
+        System.out.println(media);
+        String sinopse;
+        //Utilizando text block
+        sinopse = """
+        Filme Top Gun
+        Filme de aventura com galã dos anos 80
+        Muito bom!
+        Ano de Lançamento
+        System.out.println(sinopse);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
